86fc0d787e614b78b0db25abf473dad5e1e5f05c
plumprc <898020464@qq.com>
2021-03-02 20:14:41 +0800

    linear_regression

---
+++ lab/utils.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+
+
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+def show_fashion_mnist(images, labels):
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.view((28, 28)).numpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
