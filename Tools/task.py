3ff98766112a08205d52dd7e0ad48d7dbd8e0f9d
plumprc <898020464@qq.com>
2024-01-03 10:32:50 +0800

    task 2023

--- Tools/task.py
+++ Tools/task.py
@@ -5,7 +5,8 @@ from matplotlib.pyplot import MultipleLocator
 if __name__ == '__main__':
     x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
     # y = np.array([23, 25, 26, 1, 14, 4, 6, 13, 22, 40, 23, 26])
-    y = np.array([13, 12, 18, 35, 25, 27, 43, 20, 32, 19, 29, 23])
+    # y = np.array([13, 12, 18, 35, 25, 27, 43, 20, 32, 19, 29, 23])
+    y = np.array([29, 16, 25, 18, 45, 39, 47, 67, 22, 33, 23, 6])
     x_major_locator = MultipleLocator(1)
     y_max = np.max(y) + 2
 
